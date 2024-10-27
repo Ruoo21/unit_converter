89893111b7f6a20c4868adc36e16b8d80742a083
Ruoo21 <143183938+Ruoo21@users.noreply.github.com>
2024-10-27 07:42:06 +0200

    Add choices

--- converter.py
+++ converter.py
@@ -9,12 +9,12 @@ while True:
     else:
         break
 while True:
-    print("1. to Celsius")
-    print("2. to Fahrenheit")
-    print("3. to Gallons")
-    print("4. to Liters")
-    print("5. to Pounds")
-    print("6. to Kilograms")
+    print("1. Fahrenheit to Celsius")
+    print("2. Celsius to Fahrenheit")
+    print("3. Liters to Gallons")
+    print("4. Gallons to Liters")
+    print("5. Kilograms to Pounds")
+    print("6. Pounds to Kilograms")
     try:
         choice = int(input("Choose what you want to convert to: "))
     except ValueError:
@@ -26,3 +26,22 @@ while True:
             continue
         else:
             break
+
+
+def convert():
+    match choice:
+        case 1:
+            print(f"{chosen_number} to Celsius is {(chosen_number - 32) * 5/9:.2f}")
+        case 2:
+            print(f"{chosen_number} to Fahrenheit is {(chosen_number * 9/5) + 32:.2f}")
+        case 3:
+            print(f"{chosen_number} to Gallons is {0.264172 * chosen_number:.2f}")
+        case 4:
+            print(f"{chosen_number} to Liters is {3.785 * chosen_number:.2f}")
+        case 5:
+            print(f"{chosen_number} to Pounds is {2.20462 * chosen_number:.2f}")
+        case 6:
+            print(f"{chosen_number} to Kilograms is {0.453592 * chosen_number:.2f}")
+
+
+convert()
