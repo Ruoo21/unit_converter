4601ff812c87c47938309f88d36025c6630c9c1b
Ruoo21 <143183938+Ruoo21@users.noreply.github.com>
2024-10-27 07:21:58 +0200

    Add conversion choices

---
+++ converter.py
@@ -0,0 +1,9 @@
+print("What do you want to convert from?\n")
+
+print("1. Fahrenheit to Celsius")
+print("2. Celsius to Fahrenheit")
+print("3. Kelvin to Celsius")
+print("4. Liters to Gallons")
+print("5. Gallons to Liters")
+print("6. Kilograms to Pounds")
+print("7. Pounds to Kilograms")
