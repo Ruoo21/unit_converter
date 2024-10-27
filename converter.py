954e68c938ace635713ae46af9bf9f801ccb3c51
Ruoo21 <143183938+Ruoo21@users.noreply.github.com>
2024-10-27 07:28:50 +0200

    Add more streamlined code and options

--- converter.py
+++ converter.py
@@ -1,9 +1,28 @@
 print("What do you want to convert from?\n")
 
-print("1. Fahrenheit to Celsius")
-print("2. Celsius to Fahrenheit")
-print("3. Kelvin to Celsius")
-print("4. Liters to Gallons")
-print("5. Gallons to Liters")
-print("6. Kilograms to Pounds")
-print("7. Pounds to Kilograms")
+while True:
+    try:
+        chosen_number = float(input("Enter the number you want to convert: "))
+    except ValueError:
+        print("Make sure your choice is a number. \n")
+        continue
+    else:
+        break
+while True:
+    print("1. to Celsius")
+    print("2. to Fahrenheit")
+    print("3. to Gallons")
+    print("4. to Liters")
+    print("5. to Pounds")
+    print("6. to Kilograms")
+    try:
+        choice = int(input("Choose what you want to convert to: "))
+    except ValueError:
+        print("Make sure your choice is a number. \n")
+        continue
+    else:
+        if choice > 6 or choice == 0:
+            print("Make sure your number is between 1 and 7.\n")
+            continue
+        else:
+            break
